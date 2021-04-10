ff732c6f88c13e84cdbf13632a113241928cfc23
Tomos Fearn <tof7@aber.ac.uk>
2021-04-10 15:37:17 +0100

    added ROS node handler and while loop, commented out rate sleep

--- src/wheelchair_go_to.cpp
+++ src/wheelchair_go_to.cpp
@@ -12,6 +12,29 @@
 #include <sstream>
 using namespace std;
 
+const int DEBUG_main = 1;
+
+/**
+ * Main function that contains ROS info, subscriber callback trigger and while loop to get room name
+ *
+ * @param argc - number of arguments
+ * @param argv - content of arguments
+ * @return 0 - end of program
+ */
 int main(int argc, char** argv) {
+    ros::init(argc, argv, "wheelchair_go_to");
+    ros::NodeHandle nodeHandler;
+    //add subscribers
+    ros::Rate rate(1.0);
+
+    while (ros::ok()) {
+        //do stuff
+        if (DEBUG_main) {
+            cout << "spin \n";
+        }
+        ros::spinOnce();
+        //rate.sleep();
+    }
+
     return 0;
 }
