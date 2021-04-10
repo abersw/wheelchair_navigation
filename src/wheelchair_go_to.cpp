d4ad4620100f833baee053f18289011792a05ad9
Tomos Fearn <tof7@aber.ac.uk>
2021-04-10 15:23:47 +0100

    added wheelchair_go_to file to cmake

---
+++ src/wheelchair_go_to.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "ros/ros.h" //main ROS library
+#include <sstream>
+using namespace std;
+
+int main(int argc, char** argv) {
+    return 0;
+}
