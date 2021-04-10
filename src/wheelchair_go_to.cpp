33dba34ccdc1285b28a3ef6881bfd82817d30602
Tomos Fearn <tof7@aber.ac.uk>
2021-04-10 15:30:18 +0100

    added file version

--- src/wheelchair_go_to.cpp
+++ src/wheelchair_go_to.cpp
@@ -1,3 +1,10 @@
+/*
+ * wheelchair_go_to.cpp
+ * wheelchair_navigation
+ * version: 0.0.1 Majestic Maidenhair
+ * Status: Pre-Alpha
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
